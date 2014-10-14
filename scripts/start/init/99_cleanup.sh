caaf8c536837313b2b4d4ccc1e2089c82fa7671c
Michel Meyer <mmeyer@trobz.com>
2014-10-14 13:49:29 +0200

    fix /tmp folder permissions (seems compromised by docker)

--- scripts/start/init/99_cleanup.sh
+++ scripts/start/init/99_cleanup.sh
@@ -3,4 +3,7 @@
 info "Clean up temporary setup folder"
 rm -fr /tmp/setup
 
+# reset /tmp permissions (seems compromised by docker...)
+chown root:root /tmp && chmod 1777 /tmp 
+
 success "Temporary data removed"
