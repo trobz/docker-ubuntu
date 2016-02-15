4e52a466329bc0d7ad2991ecb97d37b0f4fe92cd
Thuan (Duong Tien) <thuan@trobz.com>
2016-02-16 00:56:33 +0700

    [FIX] mkdir should have -p option. refactor (shellcheck)

--- scripts/start/update/05_ssh.sh
+++ scripts/start/update/05_ssh.sh
@@ -8,12 +8,12 @@ if [[ $SSH_AVAILABLE -ne 0 ]]; then
     warn "Please run this image with your personal SSH keys mounted as a volume on /usr/local/ssh"
 else
     info "Copy your personal SSH keys into $USERNAME user .ssh folder..."
-    mkdir $USER_HOME/.ssh
-    /bin/rm -f $USER_HOME/.ssh/{id_rsa,id_rsa.pub,authorized_keys}
-    /bin/cp -f /usr/local/ssh/{id_rsa,id_rsa.pub} $USER_HOME/.ssh/
-    cat $USER_HOME/.ssh/id_rsa.pub > $USER_HOME/.ssh/authorized_keys 2>/dev/null
-    sudo chown -R $USERNAME:$USERNAME $USER_HOME/.ssh
-    sudo chmod -rwx,u+rwx -R $USER_HOME/.ssh
+    /bin/mkdir "$USER_HOME"/.ssh -p
+    /bin/rm -f "$USER_HOME"/.ssh/{id_rsa,id_rsa.pub,authorized_keys}
+    /bin/cp -f /usr/local/ssh/{id_rsa,id_rsa.pub} "$USER_HOME"/.ssh/
+    cat "$USER_HOME"/.ssh/id_rsa.pub > "$USER_HOME"/.ssh/authorized_keys 2>/dev/null
+    chown -R "$USERNAME":"$USERNAME" "$USER_HOME"/.ssh
+    chmod -rwx,u+rwx -R "$USER_HOME"/.ssh
 fi
 
 chmod 775 /var/run/screen
